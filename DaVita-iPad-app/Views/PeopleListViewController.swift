32106431503b004cfa26bcf775a6cc65bba3b430
jordonb2 <jordonbwn3@gmail.com>
2025-10-03 19:07:41 -0400

    feat(ui): add PeopleListViewController and wire storyboard
    - Created PeopleListViewController (Views/)
    - Set as custom class for “People List” scene
    - Embedded in UINavigationController and set title
    - Verified launch to People List screen

---
+++ DaVita-iPad-app/Views/PeopleListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PeopleListViewController.swift
+//  DaVita-iPad-app
+//
+//  Created by Jordon Bowen on 10/3/25.
+//
+
+import UIKit
+
+class PeopleListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Temporary check
+        view.backgroundColor = .systemBackground
+        title = "People List"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
