ecc4ca3e4d50e2b45d91325fb27910367078cc72
jordonb2 <jordonbwn3@gmail.com>
2025-10-03 17:24:22 -0400

    chore: initial clean Xcode iPad project (tests removed)
    - Created a new Xcode project using UIKit + Storyboard, configured for iPad.
    - Removed auto-generated unit test and UI test targets for a cleaner repo.
    - Includes AppDelegate, SceneDelegate, ViewController, Main.storyboard, and Assets setup.

---
+++ DaVita-iPad-app/DaVita-iPad-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DaVita-iPad-app
+//
+//  Created by Jordon Bowen on 10/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
